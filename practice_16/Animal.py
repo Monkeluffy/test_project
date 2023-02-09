93e22f3e9fd5ce4e25013a4e4003d15c43eb2c4d
Monkeluffy <vlad.kvartal@yandex.ru>
2023-02-09 16:35:11 +0300

    Initial commit

---
+++ practice_16/Animal.py
@@ -0,0 +1,11 @@
+from Cat_ import Cat
+cat1 = Cat('Барон', 'мальчик', 2)
+cat2 = Cat('Сэм', 'мальчик', 2)
+print('Имя:', cat1.name)
+print('Пол:', cat1.gender)
+print('Возраст:', cat1.age)
+print('Имя:', cat2.name)
+print('Пол:', cat2.gender)
+print('Возраст:', cat2.age)
+
+
